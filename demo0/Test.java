5081bf6930fb961d2b51278f596fd14c32143012
Nianko <you@example.com>
2018-03-27 15:21:24 +0800

    add interface

--- demo0/Test.java
+++ demo0/Test.java
@@ -2,7 +2,7 @@
 * @Author: nianko
 * @Date:   2018-03-26 18:10:06
 * @Last Modified by:   nianko
-* @Last Modified time: 2018-03-27 12:15:23
+* @Last Modified time: 2018-03-27 15:16:56
 */
 
 // 多态
@@ -10,7 +10,7 @@
 // 2.子类在工作时以自己的方式来实现
 // 3.子类以父类的身份出现时，子类特有的属性和方法不可以使用
 
-class Animal{
+abstract class Animal{// 抽象类
     public String name;
 
     public Animal(String name){
@@ -21,12 +21,16 @@ class Animal{
         System.out.println("大家好，我叫"+name+"，"+GetShout());
     }
 
-    protected String GetShout(){
-        return "";
-    }
-}
+    protected abstract String GetShout();  // 抽象方法
+}// 设计模式--模板方法
 
-//设计模式--模板方法
+// 抽象类
+// 1.抽象类不能实例化
+// 2.抽象方法是必须被子类重写的方法
+// 3.如果类中包含抽象方法，类就必须定义为抽象类
+
+//抽象类拥有尽可能多的共同代码，尽可能少的数据
+//树叶节点应当是具体类，树枝节点均应当是抽象类
 
 class Cat extends Animal{
     public Cat(String name){
@@ -37,7 +41,7 @@ class Cat extends Animal{
         return "我是小猫猫，喵喵喵";
     }
 
-    // public void Shout(){
+    // public void Shout(){ // 重写父类方法
     //     System.out.println("大家好，我叫"+name+"，我是小猫猫，喵喵喵");
     // }
 }
@@ -62,7 +66,26 @@ class Sheep extends Animal{
     }
 
     public String GetShout(){
+        return "我是老山羊，咩咩咩";
+    }
+}
+
+// 接口可以多继承
+// 除非实现接口的类是抽象类，否则该类要定义接口中的所有方法
+// 接口无法被实例化，但是可以被实现
+// 接口不是被类继承了，而是要被类实现
+class OldSheep extends Sheep implements Ifly{
+    public OldSheep(String name){
+        super(name);
+    }
+
+    public String GetShout(){
         return "我是老羊羔，咩咩咩";
+    }
+
+    public String Fly(String str){
+        System.out.println(str);
+        return str;
     }
 }
 
@@ -74,13 +97,18 @@ public class Test{
         new Cat("喵咂"),
         new Cat("酱肉"),
         new Cat("黑米"),
-        new Sheep("唐老鸭"),
+        new Sheep("羊羊羊")
     };
+
+    private static OldSheep old_sheep = new OldSheep("愚蠢的老羊羔");
+    private static Ifly fly = old_sheep;
 
     public static void main(String[] args){
         for(Animal animal: sign_list){
             animal.Shout();
         }
+        old_sheep.Shout();
+        fly.Fly("我是一头会飞的老羊羔, I can fly.");
     }
 }
 
